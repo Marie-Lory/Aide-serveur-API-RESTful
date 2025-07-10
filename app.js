ac6392cc4cfdff00467b17e8356a43954eeb1b51
Lory Marie <lory.marie02@outlook.fr>
2025-07-09 20:26:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ mongodb.initClientDbConnection()
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'))
-app.set('views engine', 'ejs')
+app.set('view engine', 'ejs')
 
 app.use(cors({
   exposedHeaders: ['Authorization'],
